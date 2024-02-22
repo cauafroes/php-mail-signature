d75f8b6923f294a15a274028214ccd6b5c71c59d
Cauã Fróes <bkcauafroes2012@gmail.com>
2024-02-22 12:08:44 -0300

    CHANGING Img link

--- view_signature.php
+++ view_signature.php
@@ -4,7 +4,7 @@ $img = $_GET['img'] ?? null;
 if (empty($img)) {
     http_response_code(404);
     echo 'Imagem não encontrada!';
-	exit;
+    exit;
 }
 
 //protegendo de xss e outros ataques:
@@ -23,32 +23,36 @@ if (!file_exists($file)) {
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Assinatura</title>
     <style>
-        body{
+        body {
             color: black;
             background-color: #181a2a;
             font-family: Calibri, Helvetica, sans-serif;
             text-align: center;
         }
-        h4{
+
+        h4 {
             color: #fff;
         }
-        img{
+
+        img {
             border: none;
             display: block;
             padding: 0;
             margin: 0;
         }
-        h5{
+
+        h5 {
             font-family: Arial, Helvetica, sans-serif;
         }
-        .child{
+
+        .child {
             margin: 0 auto;
             width: 80%;
             color: black;
@@ -57,7 +61,8 @@ if (!file_exists($file)) {
             border-radius: 15px;
             text-align: left;
         }
-        .buttoncopy{
+
+        .buttoncopy {
             background-color: #3486d7;
             border-radius: 10px;
             border-color: #3486d7;
@@ -72,127 +77,92 @@ if (!file_exists($file)) {
         }
     </style>
 </head>
+
 <body>
-<h4>Clique <button class="buttoncopy" onclick="copyToClipboard()">aqui</button> para copiar a assinatura e cole nas configurações de assinatura do seu outlook</h4>
-<br>
-
-<div class="child" id="signature">
-    <!--  infelizmente teve que ser em tabela, os emails tem dificuldade em mostrar padrões modernos, a maioria dos clientes de email ainda nao suportam display:flex, por exemplo   -->
-    <table
-            id="Tabela_01"
-            width="487"
-            height="153"
-            border="0"
-            cellpadding="0"
-            cellspacing="0"
-    >
-        <tr>
-            <td rowspan="2">
-                <a href="https://www.clubedeferias.com/">
-                    <img
-                            src="https://clubedeferias.com/images/emails/clubelogo.png"
-                            width="154"
-                            height="153"
-                            alt=""
-                    /></a>
-            </td>
-            <td colspan="4">
-                <a href="https://www.clubedeferias.com/">
-                    <img
-                            src="./images/<?= $img ?>"
-                            width="333"
-                            height="109"
-                            alt=""
-                    /></a>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <a href="https://www.instagram.com/oclubedeferias/">
-                    <img
-                            src="https://clubedeferias.com/images/emails/instalogo.png"
-                            width="38"
-                            height="44"
-                            alt=""
-                    /></a>
-            </td>
-            <td>
-                <a href="https://www.facebook.com/clubedeferiasstellabarros/">
-                    <img
-                            src="https://clubedeferias.com/images/emails/facelogo.png"
-                            width="32"
-                            height="44"
-                            alt=""
-                    /></a>
-            </td>
-            <td>
-                <a href="https://www.youtube.com/channel/UC3cNLeggbFCbIT3zTcGocqw">
-                    <img
-                            src="https://clubedeferias.com/images/emails/ytlogo.png"
-                            width="31"
-                            height="44"
-                            alt=""
-                    /></a>
-            </td>
-            <td>
-                <a href="https://grupoaguia.com.br/">
-                    <img
-                            src="https://images-store.us-southeast-1.linodeobjects.com/images/bottom_sig.webp"
-                            width="232"
-                            height="44"
-                            alt=""
-                    /></a>
-            </td>
-        </tr>
-    </table>
+    <h4>Clique <button class="buttoncopy" onclick="copyToClipboard()">aqui</button> para copiar a assinatura e cole nas configurações de assinatura do seu outlook</h4>
     <br>
-    <img src="footer.png" alt="">
-    <br>
-    <strong>IMPORTANTE:</strong>
-    <br>
-    Esta mensagem é destinada exclusivamente para a(s) pessoa(s) a(s) qual(is) é dirigida. Seu conteúdo é de informação
 
-    confidencial e/ou legalmente privilegiada. Se você a recebeu por engano, pedimos que retorne este e-mail imediatamente ao
+    <div class="child" id="signature">
+        <!--  infelizmente teve que ser em tabela, os emails tem dificuldade em mostrar padrões modernos, a maioria dos clientes de email ainda nao suportam display:flex, por exemplo   -->
+        <table id="Tabela_01" width="487" height="153" border="0" cellpadding="0" cellspacing="0">
+            <tr>
+                <td rowspan="2">
+                    <a href="https://www.clubedeferias.com/">
+                        <img src="https://clubedeferias.com/images/emails/clubelogo.png" width="154" height="153" alt="" /></a>
+                </td>
+                <td colspan="4">
+                    <a href="https://www.clubedeferias.com/">
+                        <img src="./images/<?= $img ?>" width="333" height="109" alt="" /></a>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <a href="https://www.instagram.com/oclubedeferias/">
+                        <img src="https://clubedeferias.com/images/emails/instalogo.png" width="38" height="44" alt="" /></a>
+                </td>
+                <td>
+                    <a href="https://www.facebook.com/clubedeferiasstellabarros/">
+                        <img src="https://clubedeferias.com/images/emails/facelogo.png" width="32" height="44" alt="" /></a>
+                </td>
+                <td>
+                    <a href="https://www.youtube.com/channel/UC3cNLeggbFCbIT3zTcGocqw">
+                        <img src="https://clubedeferias.com/images/emails/ytlogo.png" width="31" height="44" alt="" /></a>
+                </td>
+                <td>
+                    <a href="https://grupoaguia.com.br/">
+                        <img src="http://images-store.us-southeast-1.linodeobjects.com/images/imgnova.webp" width="232" height="44" alt="" /></a>
+                </td>
+            </tr>
+        </table>
+        <br>
+        <img src="footer.png" alt="">
+        <br>
+        <strong>IMPORTANTE:</strong>
+        <br>
+        Esta mensagem é destinada exclusivamente para a(s) pessoa(s) a(s) qual(is) é dirigida. Seu conteúdo é de informação
 
-    remetente, promovendo, desde logo, a eliminação do seu conteúdo em sua base de dados, registros ou sistema de controle.
+        confidencial e/ou legalmente privilegiada. Se você a recebeu por engano, pedimos que retorne este e-mail imediatamente ao
 
-    Assim como, desde já, fica notificado de abster-se a divulgar, copiar, distribuir, examinar ou, de qualquer forma, utilizar a
+        remetente, promovendo, desde logo, a eliminação do seu conteúdo em sua base de dados, registros ou sistema de controle.
 
-    informação contida nesta mensagem, por ser ilegal.
+        Assim como, desde já, fica notificado de abster-se a divulgar, copiar, distribuir, examinar ou, de qualquer forma, utilizar a
 
-
-    <br><br>
-    <strong>IMPORTANT:</strong>
-    <br>
-    This message is intended exclusively for the person (s) to whom it (s) is addressed. Its content is confidential and / or legally privileged information. If you received it by mistake, we ask that you return this e-mail to the sender immediately promoting the elimination of its content in your database, records or control system.
-
-    Furthermore, you are hereby notified to refrain from disclosing, copying, distributing, examining or, in any way, using the information contained in this message, considered to be unlawfull.
-</div>
+        informação contida nesta mensagem, por ser ilegal.
 
 
-<script>
-    //copiar tudo ao clicar
-    function copyToClipboard() {
-        let container = document.getElementById('signature');
+        <br><br>
+        <strong>IMPORTANT:</strong>
+        <br>
+        This message is intended exclusively for the person (s) to whom it (s) is addressed. Its content is confidential and / or legally privileged information. If you received it by mistake, we ask that you return this e-mail to the sender immediately promoting the elimination of its content in your database, records or control system.
 
-        //if (!navigator.clipboard){
-        let range = document.createRange();
-        window.getSelection().removeAllRanges();
-        range.selectNode(container);
-        window.getSelection().addRange(range);
-        document.execCommand('copy');
-        window.getSelection().removeAllRanges();
-        alert("Copiado para a área de transferência!");
-        /*} else{
-            navigator.clipboard.writeText(container.innerText).then(
-                function(){
-                    alert("Copiado para a área de transferência!");
-                })
-                .catch(err => function() {
-                        alert(`Erro ao copiar!: ${err}`);
-                    });
-        }*/
-    }
-</script>
+        Furthermore, you are hereby notified to refrain from disclosing, copying, distributing, examining or, in any way, using the information contained in this message, considered to be unlawfull.
+    </div>
+
+
+    <script>
+        //copiar tudo ao clicar
+        function copyToClipboard() {
+            let container = document.getElementById('signature');
+
+            //if (!navigator.clipboard){
+            let range = document.createRange();
+            window.getSelection().removeAllRanges();
+            range.selectNode(container);
+            window.getSelection().addRange(range);
+            document.execCommand('copy');
+            window.getSelection().removeAllRanges();
+            alert("Copiado para a área de transferência!");
+            /*} else{
+                navigator.clipboard.writeText(container.innerText).then(
+                    function(){
+                        alert("Copiado para a área de transferência!");
+                    })
+                    .catch(err => function() {
+                            alert(`Erro ao copiar!: ${err}`);
+                        });
+            }*/
+        }
+    </script>
 </body>
+
 </html>
